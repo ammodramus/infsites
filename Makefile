22ded9ea1d2178516c75682feb460b385a0af44f
Peter Wilton <peterrwilton@gmail.com>
2015-03-23 14:25:02 -0400

    More information with UMFPACK error

--- Makefile
+++ Makefile
@@ -22,4 +22,3 @@ csparseoptimize:
 	gcc -DCSPARSECOMPILE solver.c dataset2d.c datconfig.c dataset.c bmat.c bmat2d.c nodelist.c datconfig2d.c node.c hash.c matrix2.c murmur3.c -lm -O3 -march=native -Wall -o solver_csparse
 csparsestatic:
 	gcc -DCSPARSECOMPILE solver.c dataset2d.c datconfig.c dataset.c bmat.c bmat2d.c nodelist.c datconfig2d.c node.c hash.c matrix2.c murmur3.c -lm -O3 -msse3 --static -Wall -o solver_csparse_static
-
--- matrix.c
+++ matrix.c
@@ -75,20 +75,24 @@ void SuperEquations_add_b_value(SuperEquations * eq, double prob, int idx)
  * convert to column form. */
 void SuperEquations_solve(SuperEquations * eq)
 {
+    int i;
 	int status;
 	// convert triplet to column format
 	status = umfpack_di_triplet_to_col(eq->nrows, eq->ncols, eq->nz, eq->Ti, eq->Tj, eq->Tx, eq->Ap, eq->Ai, eq->Ax, (int *)NULL);
 	if(status != UMFPACK_OK)
 	{
 		printf("umfpack error status = %i, triplet_to_col conversion error\n", status);
-		//PERROR("UMFPACK ERROR.");
+        for(i = 0; i < eq->nz; i++)
+            printf("A[%i, %i] = %f\n", eq->Ti[i], eq->Tj[i], eq->Tx[i]);
+        printf("\n");
+		PERROR("UMFPACK ERROR: umfpack_di_triplet_to_col().");
 	}
 	// now do symbolic factorization.
 	status = umfpack_di_symbolic(eq->nrows, eq->ncols, eq->Ap, eq->Ai, eq->Ax, &(eq->symbolic), (double *)NULL, (double *)NULL);
 	if(status != UMFPACK_OK)
 	{
 		printf("umfpack error status = %i, symbolic factorization error\n", status);
-		//PERROR("UMFPACK ERROR.");
+		PERROR("UMFPACK ERROR.");
 	}
 
 	// numeric factorization
@@ -96,14 +100,18 @@ void SuperEquations_solve(SuperEquations * eq)
 	if(status != UMFPACK_OK)
 	{
 		printf("umfpack error status = %i, numeric factorization error\n", status);
-		//PERROR("UMFPACK ERROR.");
+		PERROR("UMFPACK ERROR.");
 	}
 	// solving
 	status = umfpack_di_solve(UMFPACK_A, eq->Ap, eq->Ai, eq->Ax, eq->x, eq->b, eq->numeric, (double *)NULL, (double *)NULL);
 	if(status != UMFPACK_OK)
 	{
+		printf("umfpack error status = %i, triplet_to_col conversion error\n", status);
+        for(i = 0; i < eq->nz; i++)
+            printf("A[%i, %i] = %f\n", eq->Ti[i], eq->Tj[i], eq->Tx[i]);
+        printf("\n");
 		printf("umfpack error status = %i, solving error\n", status);
-		//PERROR("UMFPACK ERROR.");
+		PERROR("UMFPACK ERROR.");
 	}
 	return;
 }
