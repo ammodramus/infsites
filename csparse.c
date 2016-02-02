767d3d15773d86c04ce8ef72808879fb2ff330aa
Peter Wilton <peterrwilton@gmail.com>
2016-02-02 17:56:26 -0500

    adding LGPL notice to csparse.[ch]

--- csparse.c
+++ csparse.c
@@ -1,3 +1,19 @@
+/*
+    This file is part of CSPARSE
+
+    CSPARSE is free software: you can redistribute it and/or modify it under
+    the terms of the Lesser GNU General Public License as published by the Free
+    Software Foundation.
+
+    CSPARSE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Lesser GNU General Public License for more details.
+
+    You should have received a copy of the Lesser GNU General Public License
+    along with this software. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 # include <stdlib.h>
 # include <limits.h>
 # include <math.h>
